8bb5c4f9b6ee3e13feeb26cfde82b5bc74adfdfd
Marouane Dbibih <marouane.dbibih@outlook.com>
2024-09-16 12:27:18 +0100

    implement the sale UI

---
+++ src/app/sale/layout.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { DefaultNavbar } from "@/components/Navbar/DefaultNavbar";
+import { SideBar } from "@/components/Sidebar/SideBar";
+import { ClientProvider } from "@/contexts/ClientProvider";
+import { SaleProvider } from "@/contexts/SaleProvider";
+import { Spinner } from "@material-tailwind/react";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export default function SaleLayout({
+  children, // will be a page or nested layout
+}: {
+  children: React.ReactNode;
+}) {
+
+
+  return (
+    <div className="flex min-h-screen py-4 pl-4 bg-blue-gray-50 w-full">
+      <SideBar />
+      <main className="flex-1 ml-[20rem] px-16 gap-8">
+        <div className="mb-8 w-full">
+          <DefaultNavbar />
+        </div>
+        <SaleProvider>{children}</SaleProvider>
+      </main>
+    </div>
+  );
+}
